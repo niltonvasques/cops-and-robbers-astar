5af6a3bcbbd00e344a9903090e63cd2ba713df28
Nilton Vasques <nilton.vasques@gmail.com>
2014-05-20 04:54:23 -0300

    Portado o código do demo 2, do policyanac para java, usando libgdx.

--- cops-and-robbers/src/br/ufba/ia/copsandrobbers/CopsAndRobbersGame.java
+++ cops-and-robbers/src/br/ufba/ia/copsandrobbers/CopsAndRobbersGame.java
@@ -1,55 +1,135 @@
 package br.ufba.ia.copsandrobbers;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import br.ufba.ia.copsandrobbers.search.AStar;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class CopsAndRobbersGame implements ApplicationListener {
+	
+	private AStar aStar = new AStar();
+	
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	
+	private TextureAtlas atlas;
+	private TextureRegion wallImage, mapImage;
+	private TextureRegion smiley, chaser;
+	
+	//-----------------------------------------------------------------------------
+	// Global variables and constants
+	//-----------------------------------------------------------------------------
+	
+	private int smileyActivated = 0;
+	private int xLoc[] = new int[4], yLoc[] = new int[4], speed[] = new int[4];
+	private long searchTime, g_showDirections=0; 
+	
+	//Declare constants
+	public static final int mapWidth = 80, mapHeight = 60, tileSize = 10, numberPeople = 3;
+	public int onClosedList = 10;
+	public static final int notfinished = 0, notStarted = 0;// path-related constants
+	public static final int found = 1, nonexistent = 2; 
+	public static final int walkable = 0, unwalkable = 1;// walkability array constants
+
+	//Create needed arrays
+	public char walkability [][] = new char[mapWidth][mapHeight];
+	int openList[] = new int[mapWidth*mapHeight+2]; //1 dimensional array holding ID# of open list items
+	int whichList[][] = new int[mapWidth+1][mapHeight+1];  //2 dimensional array used to record 
+// 		whether a cell is on the open list or on the closed list.
+	int openX[] = new int[mapWidth*mapHeight+2]; //1d array stores the x location of an item on the open list
+	int openY[] = new int[mapWidth*mapHeight+2]; //1d array stores the y location of an item on the open list
+	int parentX[][] = new int[mapWidth+1][mapHeight+1]; //2d array to store parent of each cell (x)
+	int parentY[][] = new int[mapWidth+1][mapHeight+1]; //2d array to store parent of each cell (y)
+	int Fcost[] = new int[mapWidth*mapHeight+2];	//1d array to store F cost of a cell on the open list
+	int Gcost[][] = new int[mapWidth+1][mapHeight+1]; 	//2d array to store G cost for each cell.
+	int Hcost[] = new int[mapWidth*mapHeight+2];	//1d array to store H cost of a cell on the open list
+	int pathLength[] = new int[numberPeople+1];     //stores length of the found path for critter
+	int pathLocation[] = new int[numberPeople+1];   //stores current position along the chosen path for critter		
+//	int* pathBank [numberPeople+1];
+	
+	//Path reading variables
+	int pathStatus[] = new int[numberPeople+1];
+	int xPath[] = new int[numberPeople+1];
+	int yPath[] = new int[numberPeople+1];
 	
 	@Override
 	public void create() {		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
+		camera = new OrthographicCamera(w, h);
+		camera.position.set(w/2, h/2, 0);
+		camera.update();
+		
 		batch = new SpriteBatch();
+		batch.setProjectionMatrix(camera.combined);
 		
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		LoadMapData();
+		LoadUnitData();
+		LoadGraphics();
+//		InitializePathfinder();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
 		texture.dispose();
+		
+		SaveMapData();
+		
+//		EndPathfinder();
+//		EndGraphics();
 	}
-
+	
+	//-----------------------------------------------------------------------------
+	// Name: GameMain
+	// Desc: Launch and run game.
+	//-----------------------------------------------------------------------------
+	//Main game loop
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		
+//	    while (!KeyDown(27)) //While escape key not pressed
+		if(Gdx.input.isKeyPressed(Keys.ESCAPE)) Gdx.app.exit();
+		CheckUserInput();
+		//Move smiley
+		if (smileyActivated == 1) MoveSmiley();
+	
+		//Move chasers
+		if (smileyActivated == 1)  
+			for (int ID = 2; ID <= 3; ID++) 
+				MoveChaser(ID);
+
+		
+		RenderScreen(); //draw stuff on screen
+		
 	}
 
 	@Override
@@ -64,70 +144,265 @@ public class CopsAndRobbersGame implements ApplicationListener {
 	public void resume() {
 	}
 	
-//	//-----------------------------------------------------------------------------
-//	// Global variables and constants
-//	//-----------------------------------------------------------------------------
-//	cImage* wallImage; cImage* mapImage;
-//	cImage* mousePointer; 
-//	cImage* smiley; cImage* chaser;
-//	char smileyActivated = 0;
-//	int xLoc [4]; int yLoc [4]; int speed [4];
-//	int searchTime, g_showDirections=0; 
-//
-//
-//	//-----------------------------------------------------------------------------
-//	// Function Prototypes: where necessary
-//	//-----------------------------------------------------------------------------
-//	void CheckUserInput (void);
-//	void CreateMapImage (void);
-//	void CreateWallImage (void);
-//	void DrawMap (void);
-//	void EditMap (void);
-//	void LoadGraphics (void);
-//	void LoadMapData (void);
-//	void LoadUnitData (void);
-//	void MoveChaser (int ID);
-//	void MoveSmiley (void);
-//	void MoveSprite(int ID);
-//	void RenderScreen (void);
-//	void SaveMapData (void);
-//	void ShowDirections (void);
-//
-//
-//	//-----------------------------------------------------------------------------
-//	// Name: GameMain
-//	// Desc: Launch and run game.
-//	//-----------------------------------------------------------------------------
-//	void GameMain (HWND hwnd)
-//	{
-//		Graphics (800,600,16,hwnd);
-//		LoadMapData();
-//		LoadUnitData();
-//		LoadGraphics();
-//		InitializePathfinder();
-//
-//		//Main game loop
-//	    while (!KeyDown(27)) //While escape key not pressed
-//	    {
-//			CheckUserInput();
-//
-//			//Move smiley
-//			if (smileyActivated == 1) MoveSmiley();
-//		
-//			//Move chasers
-//			if (smileyActivated == 1)  
-//				for (int ID = 2; ID <= 3; ID++) 
-//					MoveChaser(ID);
-//
-//			RenderScreen(); //draw stuff on screen
-//			CheckWinMessages();
-//		}	
-//
-//		SaveMapData();
-//		EndPathfinder();
-//		EndGraphics();
-//		return;
-//	}
+	
+	
+	//-----------------------------------------------------------------------------
+	// Function Prototypes: where necessary
+	//-----------------------------------------------------------------------------
+	
+
+	//-----------------------------------------------------------------------------
+	// Name: CheckUserInput
+	// Desc: Process key and mouse presses.
+	//-----------------------------------------------------------------------------
+	private void CheckUserInput (){
+		if (Gdx.input.isKeyPressed(Keys.ENTER))
+		{
+			smileyActivated = 1 - smileyActivated;
+			if (smileyActivated == 1) CreateMapImage();
+		}
+		if (smileyActivated == 0) EditMap();
+
+		//Show/hide directions by pressing space bar.
+		if (Gdx.input.isKeyPressed(Keys.SPACE)) g_showDirections = 1-g_showDirections;
+	}
+	
+	//-----------------------------------------------------------------------------
+	// Name: CreateMapImage
+	// Desc: Creates the map image
+	//-----------------------------------------------------------------------------
+	void CreateMapImage(){
+		Pixmap pixmap = new Pixmap(1024, 1024, Format.RGB888);
+		
+		pixmap.setColor(Color.BLUE);
+//		Color(0,0,255);//set default color to blue
+		for (int x = 0; x <= 79; x++){
+			for (int y = 0; y <= 59; y++){
+			//Draw blue walls
+			if (walkability[x][y] == unwalkable) 
+				pixmap.drawRectangle(x*10,y*10,10,10);
+		}}		
+		Texture map = new Texture(pixmap);
+		mapImage = new TextureRegion(map, 800, 600);
+	}
+	
+	void CreateWallImage(){
+		Pixmap pixmap = new Pixmap(16, 16, Format.RGB888);
+		pixmap.setColor(Color.BLUE);
+		pixmap.drawRectangle(0,0,10,10);
+		Texture map = new Texture(pixmap);
+		wallImage = new TextureRegion(map, 10,10);
+	}
+	
+	void DrawMap(SpriteBatch batch){
+		if (smileyActivated == 1) 
+			batch.draw(mapImage,0,0);
+		else
+		{
+			for (int x = 0; x <= 79; x++){
+			for (int y = 0; y <= 59; y++){
+
+				//Draw blue walls
+				if (walkability[x][y] == unwalkable) 
+					batch.draw(wallImage,x*10,y*10);
+			}}
+		}
+	}
+	
+	void EditMap(){
+		if (Gdx.input.isTouched())
+		{
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			
+			int x = ((int)touchPos.x)/10;
+			int y = ((int)touchPos.y)/10;
+			walkability[x][y] = (char)(1-walkability[x][y]);
+		}
+	}
+	
+	void LoadGraphics(){
+		atlas = new TextureAtlas("data/textures.pack");
+//		SetFont("Arial",14);
+		CreateWallImage();
+		CreateMapImage();
+
+		smiley = atlas.findRegion("smiley x 10");
+
+		chaser = atlas.findRegion("ghost x 10");
+	}
+	
+	//-----------------------------------------------------------------------------
+	// Name: LoadMapData
+	// Desc: Load any pre-existing map when launching the program.
+	//-----------------------------------------------------------------------------
+	void LoadMapData(){
+		
+		FileReader reader;
+		try {
+			reader = new FileReader(Gdx.files.internal("bin/data/myTerrainData.dat").file());
+			if (reader.ready())
+			{
+				for (int x = 0; x <= 79; x++){
+					for (int y = 0; y <= 59; y++){
+					walkability [x][y] = (char)reader.read();
+					if (walkability [x][y] > 1) walkability [x][y] = 0;
+				}}
+				reader.close();
+			}
+			else //initialize the map to completely walkable
+			{
+				for (int x = 0; x <= 79; x++){
+					for (int y = 0; y <= 59; y++){
+						walkability [x][y] = walkable;
+				}}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	//-----------------------------------------------------------------------------
+	// Name: LoadUnitData
+	// Desc: Initialize unit-related data
+	//-----------------------------------------------------------------------------
+	void LoadUnitData(){
+		xLoc[1] = 125 ; yLoc[1] = 325; //initial smiley location
+		xLoc[2] = 725 ; yLoc[2] = 325; //initial chaser location
+		xLoc[3] = 465 ; yLoc[3] = 145; //initial chaser location	
+		speed[1] = 5;//smiley speed
+		speed[2] = 3;//chaser
+		speed[3] = 2;//chaser
+	}
+	
+	//-----------------------------------------------------------------------------
+	// Name: MoveChaser
+	// Desc: This subroutine moves the chasers/ghosts around on the screen.
+	//		In this case the findPath function is accessed automatically when
+	//		a chaser reaches the end of his current path. The path info
+	//		is also updated occasionally.
+	//-----------------------------------------------------------------------------
+	void MoveChaser(int ID){
+		int targetID = 1; //ID of target (the smiley)
+
+		//1. Find Path: If smiley and chaser are not at the same location on the 
+		//			screen and no path is currently active, find a new path.
+		if (xLoc[ID] != xLoc[targetID] || yLoc[ID] != yLoc[targetID]) 
+		{
+		//If no path has been generated, generate one. Update it when
+		//the chaser reaches its fifth step on the current path.	
+		if (pathStatus[ID] == notStarted || pathLocation[ID] == 5)
+		{
+			//Generate a new path. Enter coordinates of smiley sprite (xLoc(1)/
+			//yLoc(1)) as the target.
+			pathStatus[ID] = aStar.FindPath(ID,xLoc[ID],yLoc[ID],
+				xLoc[targetID],yLoc[targetID]);
+			
+		}} 
+
+	//2.Move chaser.
+		if (pathStatus[ID] == found) MoveSprite(ID);
+	}
+	
+	void MoveSmiley(){
+		int ID = 1; //ID of smiley sprite
+
+		//1.Find Path: If smiley is active, any left or right click 
+//			on the map will find a path and make him go there.
+			if (Gdx.input.isButtonPressed(Buttons.LEFT) || Gdx.input.isButtonPressed(Buttons.RIGHT))
+			{	
+				Vector3 touchPos = new Vector3();
+				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+				camera.unproject(touchPos);
+				
+				//Call the findPath function.
+				long time1 = TimeUtils.millis();
+				pathStatus[ID] = aStar.FindPath(ID,xLoc[ID],yLoc[ID],(int)touchPos.x,(int)touchPos.y);
+				long time2 = TimeUtils.millis();
+				searchTime = time2-time1;
+			}
+
+		//2.Move smiley.
+			if (pathStatus[ID] == found) MoveSprite(ID);
+	}
+	
+	void MoveSprite(int ID){
+		//1.Read path information
+		aStar.ReadPath(ID,xLoc[ID],yLoc[ID],speed[ID]);
+
+	//2.Move sprite. xLoc/yLoc = current location of sprite. xPath and
+//		yPath = coordinates of next step on the path that were/are
+//		read using the readPath function.
+		if (xLoc[ID] > xPath[ID]) xLoc[ID] = xLoc[ID] - speed[ID];
+		if (xLoc[ID] < xPath[ID]) xLoc[ID] = xLoc[ID] + speed[ID];
+		if (yLoc[ID] > yPath[ID]) yLoc[ID] = yLoc[ID] - speed[ID];		
+		if (yLoc[ID] < yPath[ID]) yLoc[ID] = yLoc[ID] + speed[ID];
+		
+	//3.When sprite reaches the end location square	(end of its current
+//		path) ...		
+		if (pathLocation[ID] == pathLength[ID]) 
+		{
+//			Center the chaser in the square (not really necessary, but 
+//			it looks a little better for the chaser, which moves in 3 pixel
+//			increments and thus isn't always centered when it reaches its
+//			target).
+			if (Math.abs(xLoc[ID] - xPath[ID]) < speed[ID]) xLoc[ID] = xPath[ID];
+			if (Math.abs(yLoc[ID] - yPath[ID]) < speed[ID]) yLoc[ID] = yPath[ID];
+		}
+	}
+	
+	void RenderScreen(){
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		
+			batch.draw(mapImage, 0, 0);
+			
+			DrawMap(batch);
+			
+			batch.draw(smiley,xLoc[1],yLoc[1]);
+			for (char ID = 2; ID <= 3; ID++)
+				batch.draw(chaser,xLoc[ID],yLoc[ID]);
+	
+			//Show directions
+			if (g_showDirections != 0) 
+				ShowDirections();
+			else
+			{
+	//			SetFont("Arial",14);
+	//			Text(0,0,"Press space bar for directions");
+			}
+	
+			if (smileyActivated == 0) 
+			{
+	//			SetFontColor (255,0,0);
+	//			Text (0,20,"Map Edit Mode");
+	//			SetFontColor (255,255,255);
+			}
+	//		else
+	//			Text (0,20,combine "Search Time = " + searchTime + " ms");
+	//		DrawImage (mousePointer,MouseX(),MouseY());
+	//		Flip();
+		
+		batch.end();
+
+		
+	}
+	
+	void SaveMapData(){
+		
+	}
+	
+	void ShowDirections(){
+		
+	}
+
 //
 //
 //	//-----------------------------------------------------------------------------
--- cops-and-robbers/src/br/ufba/ia/copsandrobbers/search/AStar.java
+++ cops-and-robbers/src/br/ufba/ia/copsandrobbers/search/AStar.java
@@ -2,4 +2,13 @@ package br.ufba.ia.copsandrobbers.search;
 
 public class AStar {
 	private int a;
+	
+	public int FindPath(int ID, int startX, int startY, int targetX, int targetY){
+		return 1;
+	}
+	
+	public void ReadPath(int pathfinderID,int currentX,int currentY,
+			  int pixelsPerFrame){
+		
+	}
 }
